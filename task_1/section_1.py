b3e09897f7b77f75b40b13bf906151c73f941098
barisparlakk <barisparlak36@gmail.com>
2025-02-17 13:04:07 +0300

    Update task_1.py

---
+++ task_1/section_1.py
@@ -0,0 +1,26 @@
+#Toplama, cikarma, carpma, bolme, mod ve us alma iceren bir hesap makinesi yapacagiz.
+
+sayi_1 = float(input("Islem yapmak istediginiz birinci sayiyi girin: "))
+sayi_2 = float(input("Islem yapmak istediginiz ikinci sayiyi girin: "))
+operatorler = ("+","-","*","/","%","**")
+islem_operatoru = input("---Yapmak istediginiz islemi girin:--- \nToplama icin +,\nCikarma icin -,"
+                        "\nCarpma icin *,\nBolme icin /,\nMod alma icin %,\nUs alma icin **.\n "
+                        "Yapmak istediginiz islem -> ")
+
+if islem_operatoru not in operatorler:
+    print("Gecersiz bir operator girdiniz. Tekrar bir operator giriniz: ")
+    input(" Yapmak istediginiz islem -> ")
+elif islem_operatoru in operatorler:
+    if islem_operatoru == "+":
+        print(" Toplama isleminizin sonucu = ",sayi_1 + sayi_2)
+    elif islem_operatoru == "-":
+        print(" Cikarma isleminizin sonucu = ",sayi_1 - sayi_2)
+    elif islem_operatoru == "*":
+        print(" Carpma isleminizin sonucu = ",sayi_1 * sayi_2)
+    elif islem_operatoru == "/":
+        if sayi_2 == 0:
+            print("Gecersiz islem. ")
+        else:
+            print(" Bolme isleminizin sonucu = ",sayi_1 / sayi_2)
+    elif islem_operatoru == "%":
+        mod_al = input("")
